cb535ffae94540ed1da9b1d3ad2c56fde6d65fb9
mplacona <marcos.placona@gmail.com>
2016-11-16 12:02:30 +0000

    Add default route with project information

--- HomeModule.cs
+++ HomeModule.cs
@@ -13,6 +13,8 @@ namespace LoginAPI
         {
             string base64Login = EncodeToBase64(login.Email + ":" + login.Password);
 
+            Get("/", _ => "You can read more about this project in https://github.com/mplacona/LoginAPI");
+
             Post("/api/login", args =>
             {
                 var model = this.Bind<Login>();
